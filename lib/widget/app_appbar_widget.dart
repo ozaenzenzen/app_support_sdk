f0ebb9ca2e5968cdef2ff7ad210280219c4f25f8
Fauzan Akmal Mahdi <fauzan@loyalto.id>
2024-01-12 21:36:05 +0700

    add widget support

---
+++ lib/widget/app_appbar_widget.dart
@@ -0,0 +1,64 @@
+import 'package:app_support_sdk/support/app_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppAppBarWidget extends StatefulWidget implements PreferredSizeWidget {
+  final String title;
+  final Function()? onBack;
+  final List<Widget>? actions;
+  final PreferredSizeWidget? bottom;
+  final bool? automaticallyImplyLeading;
+  final double elevation;
+
+  const AppAppBarWidget({
+    super.key,
+    required this.title,
+    this.onBack,
+    this.actions,
+    this.bottom,
+    this.automaticallyImplyLeading,
+    this.elevation = 10,
+  });
+
+  @override
+  State<AppAppBarWidget> createState() => _AppAppBarWidgetState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _AppAppBarWidgetState extends State<AppAppBarWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      iconTheme: const IconThemeData(
+        color: Colors.white,
+      ),
+      backgroundColor: AppColor.primary,
+      elevation: widget.elevation,
+      shadowColor: const Color(0xff101828),
+      centerTitle: true,
+      automaticallyImplyLeading: widget.automaticallyImplyLeading ?? true,
+      leading: widget.onBack != null
+          ? InkWell(
+              onTap: widget.onBack,
+              child: const Icon(
+                Icons.arrow_back,
+                color: Colors.white,
+              ),
+            )
+          : null,
+      title: Text(
+        widget.title,
+        style: GoogleFonts.inter(
+          color: Colors.white,
+          fontWeight: FontWeight.w600,
+          fontSize: 18.sp,
+        ),
+      ),
+      actions: widget.actions,
+      bottom: widget.bottom,
+    );
+  }
+}
---
+++ lib/widget/app_loading_indicator.dart
@@ -0,0 +1,19 @@
+import 'package:app_support_sdk/support/app_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppLoadingIndicator extends StatelessWidget {
+  const AppLoadingIndicator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      height: 40.h,
+      width: 40.h,
+      child: const CircularProgressIndicator(
+        color: AppColor.primary,
+      ),
+    );
+  }
+}
